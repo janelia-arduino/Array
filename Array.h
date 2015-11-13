3c1980ba0563096149212323456b7580f0cbf3f0
Peter Polidoro <peterpolidoro@gmail.com>
2015-11-13 13:19:15 -0500

    Removed virtual keyword on methods to remove pointers and reduce size.

--- Array.h
+++ Array.h
@@ -26,21 +26,21 @@ public:
   Array();
   Array(const T &value);
   Array(const T (&values)[max_size_]);
-  virtual T& operator[](const unsigned int i);
-  virtual T& at(const unsigned int i);
-  virtual T& front();
-  virtual T& back();
-  virtual void clear();
-  virtual void fill(const T &value);
-  virtual void fill(const T (&values)[max_size_]);
-  virtual void assign(const unsigned int n, const T &value);
-  virtual void assign(const unsigned int n, const T values[]);
-  virtual void push_back(const T &value);
-  virtual void pop_back();
-  virtual unsigned int size();
-  virtual unsigned int max_size();
-  virtual bool empty();
-  virtual bool full();
+  T& operator[](const unsigned int i);
+  T& at(const unsigned int i);
+  T& front();
+  T& back();
+  void clear();
+  void fill(const T &value);
+  void fill(const T (&values)[max_size_]);
+  void assign(const unsigned int n, const T &value);
+  void assign(const unsigned int n, const T values[]);
+  void push_back(const T &value);
+  void pop_back();
+  unsigned int size();
+  unsigned int max_size();
+  bool empty();
+  bool full();
 
 private:
   T values_[max_size_];
